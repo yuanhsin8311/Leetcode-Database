68852e01ab4b2a4695973d80a99b6a84bd3abb3a
yuanhsin8311 <yuanhsin8311>
2024-08-05 09:56:25 +0000

    commit

---
+++ 3198_Find_Cities_In_Each_States.sql
@@ -0,0 +1,58 @@
+/*----------------------------------------------------------------------------
+(Easy)
+Table: cities
+
++-------------+---------+
+| Column Name | Type    | 
++-------------+---------+
+| state       | varchar |
+| city        | varchar |
++-------------+---------+
+(state, city) is the primary key (combination of columns with unique values) for this table.
+Each row of this table contains the state name and the city name within that state.
+
+Write a solution to find all the cities in each state and combine them into a single comma-separated string.
+Return the result table ordered by state in ascending order.
+
+The result format is in the following example.
+
+Example:
+
+cities table:
++-------------+---------------+
+| state       | city          |
++-------------+---------------+
+| California  | Los Angeles   |
+| California  | San Francisco |
+| California  | San Diego     |
+| Texas       | Houston       |
+| Texas       | Austin        |
+| Texas       | Dallas        |
+| New York    | New York City |
+| New York    | Buffalo       |
+| New York    | Rochester     |
++-------------+---------------+
+Output:
++-------------+---------------------------------------+
+| state       | cities                                |
++-------------+---------------------------------------+
+| California  | Los Angeles, San Diego, San Francisco |
+| New York    | Buffalo, New York City, Rochester     |
+| Texas       | Austin, Dallas, Houston               |
++-------------+---------------------------------------+
+Explanation:
+
+California: All cities ("Los Angeles", "San Diego", "San Francisco") are listed in a comma-separated string.
+New York: All cities ("Buffalo", "New York City", "Rochester") are listed in a comma-separated string.
+Texas: All cities ("Austin", "Dallas", "Houston") are listed in a comma-separated string.
+Note: The output table is ordered by the state name in ascending order.
+----------------------------------------------------------------------------------*/
+select state, STRING_AGG(city, ', ' ORDER BY city) AS cities
+from cities
+group by state
+order by 1
+
+
+
+
+
